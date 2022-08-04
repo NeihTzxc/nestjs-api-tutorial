876698f9ea1524fbcdc4d0454cef916364cbeb11
NeihTzxc <45267625+NeihTzxc@users.noreply.github.com>
2022-08-04 11:03:29 +0700

    e2e tests with pactumjs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { AuthDto } from './dto/auth.dto';
 import { ForbiddenException, Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+// import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from '../../src/prisma/prisma.service';
 import * as argon from 'argon2'
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { JwtService } from '@nestjs/jwt';
